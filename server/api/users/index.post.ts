e46a1e7c5cf807890addfdc937490e635bfc88d2
Tor Magne Brekken <tormage@hotmail.com>
2026-09-07 10:15:56 +0200

    feat: implement user creation API and add dashboard user page

--- server/api/users/index.post.ts
+++ server/api/users/index.post.ts
@@ -1,7 +1,19 @@
+import { usersTable } from '~~/server/db/schema';
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
   const hashedPassword = await hashPassword(body.password);
 
-  const [user] = await useDrizzle;
+  const [user] = await useDrizzle()
+    .insert(usersTable)
+    .values({
+      farmId: body.farmid,
+      email: body.email,
+      passwordHash: hashedPassword,
+      role: body.role,
+    })
+    .returning();
+
+  return user;
 });
