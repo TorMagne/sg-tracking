770baa073dbb5777acc3aa5505c0e743809dacfa
Tor Magne Brekken <tormage@hotmail.com>
2026-09-08 13:50:03 +0200

    feat: add user creation form with email, password, and role selection

--- app/pages/dashboard/users.vue
+++ app/pages/dashboard/users.vue
@@ -3,10 +3,28 @@
     <div class="card bg-base-100 w-96 shadow-sm mt-4">
       <div class="card-body">
         <h2 class="card-title">Lag en bruker</h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are title and actions
-          parts
-        </p>
+        <form class="mb-2">
+          <!-- e-post -->
+          <fieldset class="fieldset mb-2">
+            <legend class="fieldset-legend">E-post</legend>
+            <input type="email" class="input" placeholder="e-post" />
+          </fieldset>
+          <!-- passord -->
+          <fieldset class="fieldset mb-2">
+            <legend class="fieldset-legend">Password</legend>
+            <input type="password" class="input" placeholder="passord" />
+          </fieldset>
+          <!-- role -->
+          <fieldset class="fieldset mb-2">
+            <legend class="fieldset-legend">Velg rolle</legend>
+            <select class="select">
+              <option disabled selected>Velg rolle</option>
+              <option>user</option>
+              <option>visitor</option>
+              <option>admin</option>
+            </select>
+          </fieldset>
+        </form>
         <div class="card-actions">
           <button class="btn btn-primary">Lag bruker</button>
         </div>
